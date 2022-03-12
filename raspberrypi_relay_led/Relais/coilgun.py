ea302cc398b0b792f0aa52735d329989fa95561a
Leopixelo <kleinerbear2006@gmail.com>
2022-03-12 18:10:48 +0100

    linux support

--- raspberrypi_relay_led/Relais/coilgun.py
+++ raspberrypi_relay_led/Relais/coilgun.py
@@ -1,9 +1,26 @@
 from os import system, get_terminal_size, name as os_name
-from sys import stdout
-from msvcrt import getwch  # , kbhit
+from sys import stdout, stdin
 from time import sleep
 from json import load, dump
 # from RPi import GPIO
+
+if os_name == "nt":
+    from msvcrt import getwch as getwch_msvcrt
+
+    getwch = getwch_msvcrt
+
+else:
+    from termios import tcgetattr, tcsetattr, TCSADRAIN
+    from tty import setraw
+
+    stdin_fileno = stdin.fileno()
+    old_terminal_settings = tcgetattr(stdin_fileno)
+
+    setraw(stdin.fileno())
+
+    def getwch():
+        return stdin.read(1)
+
 
 system("")  # make ANSI escape sequence get processed on windows
 
@@ -11,22 +28,26 @@ system("")  # make ANSI escape sequence get processed on windows
 
 ESC = "\x1b"
 
-KEYS_SPECIAL = ("\x00", "\xe0")
+KEY_INDEX = 0 if os_name == "nt" else 1
 
-KEY_CTRL_C = "\x03"
-KEY_BACKSPACE = "\x08"
-KEY_ENTER = "\r"
+KEYS_SPECIAL_TO_1 = (("\x00", "\xe0"), (ESC, ))
+KEYS_SPECIAL_1 = ((), ("["))
+KEYS_SPECIAL_TO_2 = ((), ("1"))
+KEYS_SPECIAL_2 = ((), (";", "5"))
 
-KEY_UP = "H"
-KEY_DOWN = "P"
-KEY_LEFT = "K"
-KEY_RIGHT = "M"
-KEY_CTRL_UP = "\x8d"
-KEY_CTRL_DOWN = "\x91"
-KEY_CTRL_LEFT = "s"
-KEY_CTRL_RIGHT = "t"
-KEY_POS1 = "G"
-KEY_END = "O"
+KEY_UP = ("H", "A")
+KEY_DOWN = ("P", "B")
+KEY_LEFT = ("K", "D")
+KEY_RIGHT = ("M", "C")
+KEY_CTRL_UP = ("\x8d", None)
+KEY_CTRL_DOWN = ("\x91", None)
+KEY_CTRL_LEFT = ("s", None)
+KEY_CTRL_RIGHT = ("t", None)
+KEY_POS1 = ("G", "H")
+KEY_END = ("O", "F")
+KEY_BACKSPACE = ("\x08", "\x7f")
+KEY_ENTER = ("\r", "\r")
+KEY_CTRL_C = ("\x03", "\x03")
 
 LINE_H = "─"
 LINE_V = "│"
@@ -290,6 +311,7 @@ def draw_command_bar():
 
 
 def draw_screen():
+    set_cursor_visible(False)
     clear()
     update_tty_size()
     draw_titlebar()
@@ -325,7 +347,7 @@ def run_command():
     global time_values, title, command
 
     if command in ("q", "quit"):
-        exit()
+        return True
 
     elif command in ("f", "fire"):
         fire()
@@ -396,168 +418,242 @@ def run_command():
         return "Error - unknown command"
 
 
-set_cursor_visible(False)
+def command_bar_cursor_move(step):
+    global command_bar_cursor_pos
 
-clear()
+    command_bar_cursor_pos += step
+
+    if command_bar_cursor_pos > len(command):
+        command_bar_cursor_pos = len(command)
+    elif command_bar_cursor_pos < 0:
+        command_bar_cursor_pos = 0
+
+    draw_command_bar()
+
+
+def adjust_time(time_index, digit, direction):
+    time_values[time_index] += 10**(-digit + 2) * direction
+    time_values[time_index] = bound_time(time_values[time_index])
+
+    draw_value(time_index)
+
+
+def adjust_all_times(time_index, digit, direction):
+    val = 10**(-digit + 2) * direction
+
+    for i in range(time_index % 2, COIL_NUM * 2 - 1, 2):
+        time_values[i] += val
+        time_values[i] = bound_time(time_values[i])
+
+        draw_value(i)
+
+
+def command_insert_key(key):
+    global command
+
+    if command_bar_cursor_pos == 0:
+        command += key
+    else:
+        command = command[:-command_bar_cursor_pos] + key + command[-command_bar_cursor_pos:]
+
+    draw_command_bar()
+
+
+def key_pressed_up():
+    if command_bar_active:
+        pass
+
+    else:
+        adjust_time(control_arrows_pos_value, control_arrows_pos_digit, 1)
+
+
+def key_pressed_down():
+    if command_bar_active:
+        pass
+
+    else:
+        adjust_time(control_arrows_pos_value, control_arrows_pos_digit, -1)
+
+
+def key_pressed_left():
+    if command_bar_active:
+        command_bar_cursor_move(1)
+
+    else:
+        global control_arrows_pos_value, control_arrows_pos_digit
+
+        draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, False)
+
+        if control_arrows_pos_digit == 0:
+            if control_arrows_pos_value == 0:
+                control_arrows_pos_value = COIL_NUM * 2 - 2
+            else:
+                control_arrows_pos_value -= 1
+            control_arrows_pos_digit = TIME_VALUE_DIGITS
+        else:
+            control_arrows_pos_digit -= 1
+
+        draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, True)
+
+
+def key_pressed_right():
+    if command_bar_active:
+        command_bar_cursor_move(-1)
+
+    else:
+        global control_arrows_pos_value, control_arrows_pos_digit
+
+        draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, False)
+
+        if control_arrows_pos_digit == TIME_VALUE_DIGITS:
+            if control_arrows_pos_value == COIL_NUM * 2 - 2:
+                control_arrows_pos_value = 0
+            else:
+                control_arrows_pos_value += 1
+            control_arrows_pos_digit = 0
+        else:
+            control_arrows_pos_digit += 1
+
+        draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, True)
+
+
+def key_pressed_ctrl_up():
+    if command_bar_active:
+        pass
+    else:
+        adjust_all_times(control_arrows_pos_value, control_arrows_pos_digit, 1)
+
+
+def key_pressed_ctrl_down():
+    if command_bar_active:
+        pass
+    else:
+        adjust_all_times(control_arrows_pos_value, control_arrows_pos_digit, -1)
+
+
+def key_pressed_ctrl_left():
+    if command_bar_active:
+        pass
+    else:
+        global control_arrows_pos_value
+
+        draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, False)
+
+        if control_arrows_pos_value == 0:
+            control_arrows_pos_value = COIL_NUM * 2 - 2
+        else:
+            control_arrows_pos_value -= 1
+
+        draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, True)
+
+
+def key_pressed_ctrl_right():
+    if command_bar_active:
+        pass
+    else:
+        global control_arrows_pos_value
+
+        draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, False)
+
+        if control_arrows_pos_value == COIL_NUM * 2 - 2:
+            control_arrows_pos_value = 0
+        else:
+            control_arrows_pos_value += 1
+
+        draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, True)
+
+
+def key_pressed_pos1():
+    if command_bar_active:
+        global command_bar_cursor_pos
+
+        command_bar_cursor_pos = len(command)
+        draw_command_bar()
+
+
+def key_pressed_end():
+    if command_bar_active:
+        global command_bar_cursor_pos
+
+        command_bar_cursor_pos = 0
+        draw_command_bar()
+
+
+def key_pressed_esc():
+    global command, command_bar_active
+
+    if command_bar_active:
+
+        command_bar_active = False
+        command = ""
+        draw_command_bar()
+
+
+def key_pressed_backspace():
+    if command_bar_active:
+        global command
+
+        if command_bar_cursor_pos == 0:
+            command = command[:-1]
+
+        else:
+            command = command[:-(command_bar_cursor_pos + 1)] + command[-command_bar_cursor_pos:]
+
+        draw_command_bar()
+
+
+def key_pressed_enter():
+    global command, command_bar_active
+
+    if command_bar_active:
+
+        exec_return = run_command()
+
+        if exec_return is True:
+            return True
+
+        elif exec_return is None:
+            command = ""
+
+        else:
+            command = " " + exec_return
+
+        command_bar_active = False
+        draw_command_bar()
+        command = ""
+
 
 draw_screen()
 
-special_key_comming = False
+special_key_comming = 0
 
 while True:
     key = getwch()
 
-    if key in KEYS_SPECIAL:
-        special_key_comming = True
-        continue
+    if special_key_comming == 0:
+        if key in KEYS_SPECIAL_TO_1[KEY_INDEX]:
+            special_key_comming = 1
 
-    if special_key_comming:
-        special_key_comming = False
+            continue
 
-        if command_bar_active:
-            if key == KEY_LEFT:
-                command_bar_cursor_pos += 1
-
-                if command_bar_cursor_pos > len(command):
-                    command_bar_cursor_pos = len(command)
-
-                draw_command_bar()
-
-            elif key == KEY_RIGHT:
-                command_bar_cursor_pos -= 1
-
-                if command_bar_cursor_pos < 0:
-                    command_bar_cursor_pos = 0
-
-                draw_command_bar()
-
-            elif key == KEY_POS1:
-                command_bar_cursor_pos = len(command)
-                draw_command_bar()
-
-            elif key == KEY_END:
-                command_bar_cursor_pos = 0
-                draw_command_bar()
-
-        else:
-            if key == KEY_UP:
-                time_values[control_arrows_pos_value] += 10**(-control_arrows_pos_digit + 2)
-                time_values[control_arrows_pos_value] = bound_time(time_values[control_arrows_pos_value])
-
-                draw_value(control_arrows_pos_value)
-
-            elif key == KEY_DOWN:
-                time_values[control_arrows_pos_value] -= 10**(-control_arrows_pos_digit + 2)
-                time_values[control_arrows_pos_value] = bound_time(time_values[control_arrows_pos_value])
-
-                draw_value(control_arrows_pos_value)
-
-            elif key == KEY_LEFT:
-                draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, False)
-
-                if control_arrows_pos_digit == 0:
-                    if control_arrows_pos_value == 0:
-                        control_arrows_pos_value = COIL_NUM * 2 - 2
-                    else:
-                        control_arrows_pos_value -= 1
-                    control_arrows_pos_digit = TIME_VALUE_DIGITS
-                else:
-                    control_arrows_pos_digit -= 1
-
-                draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, True)
-
-            elif key == KEY_RIGHT:
-                draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, False)
-
-                if control_arrows_pos_digit == TIME_VALUE_DIGITS:
-                    if control_arrows_pos_value == COIL_NUM * 2 - 2:
-                        control_arrows_pos_value = 0
-                    else:
-                        control_arrows_pos_value += 1
-                    control_arrows_pos_digit = 0
-                else:
-                    control_arrows_pos_digit += 1
-
-                draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, True)
-
-            elif key == KEY_CTRL_UP:
-                val = 10**(-control_arrows_pos_digit + 2)
-
-                for i in range(control_arrows_pos_value % 2, COIL_NUM * 2 - 1, 2):
-                    time_values[i] += val
-                    time_values[i] = bound_time(time_values[i])
-
-                    draw_value(i)
-
-            elif key == KEY_CTRL_DOWN:
-                val = 10**(-control_arrows_pos_digit + 2)
-
-                for i in range(control_arrows_pos_value % 2, COIL_NUM * 2 - 1, 2):
-                    time_values[i] -= val
-                    time_values[i] = bound_time(time_values[i])
-
-                    draw_value(i)
-
-            elif key == KEY_CTRL_LEFT:
-                draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, False)
-
-                if control_arrows_pos_value == 0:
-                    control_arrows_pos_value = COIL_NUM * 2 - 2
-                else:
-                    control_arrows_pos_value -= 1
-
-                draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, True)
-
-            elif key == KEY_CTRL_RIGHT:
-                draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, False)
-
-                if control_arrows_pos_value == COIL_NUM * 2 - 2:
-                    control_arrows_pos_value = 0
-                else:
-                    control_arrows_pos_value += 1
-
-                draw_control_arrows(control_arrows_pos_value, control_arrows_pos_digit, True)
-
-            # else:
-            #     print("special: ", bytes(key, "unicode_escape"))
-
-    else:
         if command_bar_active:
             if key == ESC:
-                command_bar_active = False
-                command = ""
-                draw_command_bar()
+                key_pressed_esc()
 
-            elif key == KEY_BACKSPACE:
-                if command_bar_cursor_pos == 0:
-                    command = command[:-1]
-                else:
-                    command = command[:-(command_bar_cursor_pos + 1)] + command[-command_bar_cursor_pos:]
+            elif key == KEY_BACKSPACE[KEY_INDEX]:
+                key_pressed_backspace()
 
-                draw_command_bar()
+            elif key == KEY_ENTER[KEY_INDEX]:
+                exit_now = key_pressed_enter()
 
-            elif key == KEY_ENTER:
-                command = run_command()
-
-                if not command:
-                    command = ""
-                else:
-                    command = " " + command
-
-                command_bar_active = False
-                draw_command_bar()
-                command = ""
+                if exit_now:
+                    break
 
             else:
-                if command_bar_cursor_pos == 0:
-                    command += key
-                else:
-                    command = command[:-command_bar_cursor_pos] + key + command[-command_bar_cursor_pos:]
-
-                draw_command_bar()
+                command_insert_key(key)
 
         else:
-            if key == "q" or key == KEY_CTRL_C:
+            if key == "q" or key == KEY_CTRL_C[KEY_INDEX]:
                 break
 
             elif key == "f":
@@ -573,4 +669,78 @@ while True:
             # else:
             #     print("normal: ", bytes(key, "unicode_escape"))
 
+    if special_key_comming == 1:
+        if key in KEYS_SPECIAL_1[KEY_INDEX]:
+            continue
+
+        elif key in KEYS_SPECIAL_TO_2[KEY_INDEX]:
+            special_key_comming = 2
+
+            continue
+
+        if key == KEY_UP[KEY_INDEX]:
+            key_pressed_up()
+
+        elif key == KEY_DOWN[KEY_INDEX]:
+            key_pressed_down()
+
+        elif key == KEY_LEFT[KEY_INDEX]:
+            key_pressed_left()
+
+        elif key == KEY_RIGHT[KEY_INDEX]:
+            key_pressed_right()
+
+        elif key == KEY_CTRL_UP[KEY_INDEX]:
+            key_pressed_ctrl_up()
+
+        elif key == KEY_CTRL_DOWN[KEY_INDEX]:
+            key_pressed_ctrl_down()
+
+        elif key == KEY_CTRL_LEFT[KEY_INDEX]:
+            key_pressed_ctrl_left()
+
+        elif key == KEY_CTRL_RIGHT[KEY_INDEX]:
+            key_pressed_ctrl_right()
+
+        elif key == KEY_POS1[KEY_INDEX]:
+            key_pressed_pos1()
+
+        elif key == KEY_END[KEY_INDEX]:
+            key_pressed_end()
+
+        elif key == ESC:
+            key_pressed_esc()
+
+        # else:
+        #     print("special 1: ", bytes(key, "unicode_escape"))
+
+        special_key_comming = 0
+
+    elif special_key_comming == 2:
+
+        if key in KEYS_SPECIAL_2[KEY_INDEX]:
+            continue
+
+        if key == KEY_UP[KEY_INDEX]:
+            key_pressed_ctrl_up()
+
+        elif key == KEY_DOWN[KEY_INDEX]:
+            key_pressed_ctrl_down()
+
+        elif key == KEY_LEFT[KEY_INDEX]:
+            key_pressed_ctrl_left()
+
+        elif key == KEY_RIGHT[KEY_INDEX]:
+            key_pressed_ctrl_right()
+
+        # else:
+        #     print("special 2: ", bytes(key, "unicode_escape"))
+
+        special_key_comming = 0
+
 set_cursor_pos(tty_height - 1, 0)
+
+set_cursor_visible(True)
+
+if os_name != "nt":
+    tcsetattr(stdin_fileno, TCSADRAIN, old_terminal_settings)
